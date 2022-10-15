4c72d841fdf4aac2d6368c26a1b30f5e05888cb5
ewang6652 <enjiewang6652@gmail.com>
2022-10-15 15:30:18 -0400

    simple search

---
+++ my-app/src/search.js
@@ -0,0 +1,33 @@
+function searchSongs(songs, times) {
+    let time = convertToSecond(times)
+    let length = songs.length
+    var showerSong = []
+    var i = true
+    var timer = 0
+    let safety = 0
+    while(i) {
+        var song = getRandomInt(0, length-1)
+        if(songs[song].duration_ms/1000+timer+2<time) {
+            showerSong.append(songs[song])
+            timer += songs[song].duration_ms/1000
+        }
+        if(timer+2>time && timer-2<time) {
+            i = false
+        }
+        safety += 1
+        if(safety>time*10) {
+            i = false
+        }
+    }
+    return showerSong
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function convertToSecond(time) {
+    return time*60
+}
