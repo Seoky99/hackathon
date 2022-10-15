e68ffb835ab5c5132c7b69b02857611665fbbd65
thisjustin123 <jjg283@cornell.edu>
2022-10-15 16:21:17 -0400

    Add greedy track sublist generator function

--- my-app/src/search.js
+++ my-app/src/search.js
@@ -7,23 +7,23 @@ export function unsafeSearchSongs(songs, times) {
     var i = true
     var timer = 0
     var safety = 0
-    while(i) {
-        var songInt = getRandomInt(0, lengthCopy-1)
-        if(songCopy[songInt].duration_ms/1000+timer+2<time) {
+    while (i) {
+        var songInt = getRandomInt(0, lengthCopy - 1)
+        if (songCopy[songInt].duration_ms / 1000 + timer + 2 < time) {
             showerSong.push(songCopy[songInt])
-            songCopy.splice(songInt,1)
-            timer += songs[songInt].duration_ms/1000
+            songCopy.splice(songInt, 1)
+            timer += songs[songInt].duration_ms / 1000
             lengthCopy -= 1
         }
         safety += 1
-        if (safety>length*2) {
-            var song = getRandomInt(0, length-1)
-            if(songs[song].duration_ms/1000+timer+2<time) {
+        if (safety > length * 2) {
+            var song = getRandomInt(0, length - 1)
+            if (songs[song].duration_ms / 1000 + timer + 2 < time) {
                 showerSong.push(songs[song])
-                timer += songs[song].duration_ms/1000
-            }    
+                timer += songs[song].duration_ms / 1000
+            }
         }
-        if(timer+2>time && timer-2<time) {
+        if (timer + 2 > time && timer - 2 < time) {
             i = false
         }
     }
@@ -37,17 +37,17 @@ export function searchSongs(songs, times) {
     var i = true
     var timer = 0
     let safety = 0
-    while(i) {
-        var song = getRandomInt(0, length-1)
-        if(songs[song].duration_ms/1000+timer+2<time) {
+    while (i) {
+        var song = getRandomInt(0, length - 1)
+        if (songs[song].duration_ms / 1000 + timer + 2 < time) {
             showerSong.push(songs[song])
-            timer += songs[song].duration_ms/1000
+            timer += songs[song].duration_ms / 1000
         }
-        if(timer+2>time && timer-2<time) {
+        if (timer + 2 > time && timer - 2 < time) {
             i = false
         }
         safety += 1
-        if(safety>time*10) {
+        if (safety > time * 10) {
             i = false
         }
     }
@@ -61,5 +61,27 @@ function getRandomInt(min, max) {
 }
 
 function convertToSecond(time) {
-    return time*60
+    return time * 60
+}
+
+/**
+ * [ getCloserSongs(lst, current, goal) ] returns a subarray of [lst] consisting
+ * of tracks you can add such that the current time will be greedily brought
+ * closer to the goal.
+ * 
+ * @param {track[]} lst The lst of songs to consider. 
+ * @param {float} current The current time.
+ * @param {float} goal The goal time.
+ * 
+ * @return A list [l] of all songs in [lst] that, if added, would bring the current
+ * time closer to the goal. 0 <= l.length <= lst.length
+ */
+function getCloserSongs(lst, current, goal) {
+    const tracks = []
+    for (let i = 0; i < lst.length; i++) {
+        if (Math.abs(current - goal) > Math.abs(((lst[i].duration_ms / 1000) + current) - goal)) {
+            tracks.push(lst[i])
+        }
+    }
+    return tracks
 }
