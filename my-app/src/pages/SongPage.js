cb71c4a24986640e13747f78970cd54d8ff85cdf
thisjustin123 <jjg283@cornell.edu>
2022-10-15 22:29:55 -0400

    Add song concluded callback

--- my-app/src/pages/SongPage.js
+++ my-app/src/pages/SongPage.js
@@ -3,6 +3,12 @@ import SpotifyPlayer from "react-spotify-web-playback"
 import { fetchUser, fetchUserPlaylists, flattenSongs, getAllPlaylists, mapUris } from "../Spinfo";
 
 const SongPage = ({ currentSong, songArtist, albumCover }) => {
+    const callback = (state) => {
+        if (state.progressMs == 0 && !state.isPlaying && state.nextTracks.length == 0) {
+            console.log("Finished!")
+        }
+    }
+
     return (
         <div>
             <h1 className="songpage-header">Currently Playing:</h1>
@@ -20,6 +26,7 @@ const SongPage = ({ currentSong, songArtist, albumCover }) => {
             </h2>
             <SpotifyPlayer
                 autoPlay={true}
+                callback={callback}
                 token={localStorage.getItem("access_token")}
                 uris={mapUris(JSON.parse(localStorage.getItem("gen_playlist")))}
                 name="Shower Music Player"
